e20ba6b3c236728e3792b4fa11b31d0c3004abf3
Jainethin M <jainethin59@gmail.com>
2024-08-02 10:32:16 +0530

    Time: 0 ms (100%), Space: 8 MB (9.17%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.c
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.c
@@ -1,17 +1,11 @@
-void rev(char a[],int n){
-    for(int i=0;i<n/2+1;i++){
-        char t=a[i];
-        a[i]=a[n-i];
-        a[n-i]=t;
-    }
-}
 char* reversePrefix(char* word, char ch) {
-    for(int i=0;i<strlen(word);i++){
-        if(word[i]==ch){
-            rev(word,i);
-            break;
-        }
+    char* r=strchr(word, ch);
+    if (r==NULL) return word;
+    int r1=r-word, r0=(r1-1)/2;
+    for(int i=0; i<=r0; i++){
+        char tmp=word[i];
+        word[i]=word[r1-i];
+        word[r1-i]=tmp;
     }
-    
     return word;
 }
