7d8aaf2cb137be537a403de01e1ecab22436b6f2
Jainethin M <jainethin59@gmail.com>
2024-08-02 10:37:13 +0530

    Time: 3 ms (49.45%), Space: 8.3 MB (6.38%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.c
@@ -0,0 +1,14 @@
+int numIdenticalPairs(int* nums, int numsSize) {
+    int c=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        for(int j=i+1;j<numsSize;j++)
+        {
+            if(nums[i]==nums[j])
+            {
+                c++;
+            }
+        }
+    }
+    return c;
+}
