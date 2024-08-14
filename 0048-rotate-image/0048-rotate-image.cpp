7898a93b4b5565ac0dcc1d3a170857532d801ee7
Jainethin M <jainethin59@gmail.com>
2024-08-14 22:39:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (48.9%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<i;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+            
+        }
+
+        return ;
+    }
+};
