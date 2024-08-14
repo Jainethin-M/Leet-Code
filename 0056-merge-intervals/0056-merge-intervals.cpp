5e31cc7ad38c1483405dd572b48d512aa79ee92b
Jainethin M <jainethin59@gmail.com>
2024-08-14 22:19:44 +0530

    Time: 28 ms (26.11%), Space: 23 MB (49.36%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        auto vit=intervals.begin();
+        ans.push_back(*vit);
+        vit++;
+        for(int i=1;i<intervals.size();i++){
+            if(ans.back().back()>=intervals[i].front())
+            {
+                ans.back().back()=max(intervals[i].back(),ans.back().back());
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        
+        }
+        // printf("%d",ans.back().back());
+        return ans;
+    }
+};
