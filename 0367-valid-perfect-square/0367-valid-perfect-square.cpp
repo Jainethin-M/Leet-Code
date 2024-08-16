0d1aa1a1fd1fc9cd50d2c2933ebc954aa37ea213
Jainethin M <jainethin59@gmail.com>
2024-08-16 22:46:03 +0530

    Time: 0 ms (100%), Space: 7.4 MB (26.3%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        float sr=sqrt(num);
-        cout<<sr;
+        
         if((int)sqrt(num)==sqrt(num))return true;
         else return false;
     }
